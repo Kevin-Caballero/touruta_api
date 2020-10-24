24de8b0386939a951e37bda1c157ee7a925796ba
Kevin <caballero.kevin94@gmail.com>
2020-10-24 19:01:00 +0200

    reestructuracion entityConfiguration

--- touruta_api/touruta_infrastructure/Data/TourutaContext.cs
+++ touruta_api/touruta_infrastructure/Data/TourutaContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Touruta.Core.Data;
+using Touruta.Infrastructure.Data.Configurations;
 
 namespace Touruta.Infrastructure.Data
 {
@@ -23,112 +24,10 @@ namespace Touruta.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Comment>(entity =>
-            {
-                entity.HasKey(e => e.IdComment)
-                    .HasName("PK_Comentario");
-
-                entity.Property(e => e.IdComment).ValueGeneratedNever();
-
-                entity.Property(e => e.Date).HasColumnType("datetime");
-
-                entity.Property(e => e.Description)
-                    .IsRequired()
-                    .HasMaxLength(500)
-                    .IsUnicode(false);
-
-                entity.HasOne(d => d.IdTourNavigation)
-                    .WithMany(p => p.Comments)
-                    .HasForeignKey(d => d.IdTour)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Comments_Tours");
-
-                entity.HasOne(d => d.IdUserNavigation)
-                    .WithMany(p => p.Comments)
-                    .HasForeignKey(d => d.IdUser)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Comments_Users");
-            });
-
-            modelBuilder.Entity<Role>(entity =>
-            {
-                entity.HasKey(e => e.IdRol);
-
-                entity.Property(e => e.IdRol).ValueGeneratedNever();
-
-                entity.Property(e => e.Rol)
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<Tour>(entity =>
-            {
-                entity.HasKey(e => e.IdTour)
-                    .HasName("PK_Publicacion");
-
-                entity.Property(e => e.Audio)
-                    .HasMaxLength(500)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Date).HasColumnType("datetime");
-
-                entity.Property(e => e.Description)
-                    .IsRequired()
-                    .HasMaxLength(1000)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Image)
-                    .HasMaxLength(500)
-                    .IsUnicode(false);
-
-                entity.HasOne(d => d.IdUserNavigation)
-                    .WithMany(p => p.Tours)
-                    .HasForeignKey(d => d.IdUser)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Tours_Users");
-            });
-
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasKey(e => e.IdUser)
-                    .HasName("PK_Usuario");
-
-                entity.Property(e => e.BirthDate).HasColumnType("date");
-
-                entity.Property(e => e.Email)
-                    .IsRequired()
-                    .HasMaxLength(30)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.LastName)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Phone)
-                    .HasMaxLength(10)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.UserName)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.HasOne(d => d.IdRolNavigation)
-                    .WithMany(p => p.Users)
-                    .HasForeignKey(d => d.IdRol)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Users_Roles");
-            });
-
-            OnModelCreatingPartial(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CommentConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new TourConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
         }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
