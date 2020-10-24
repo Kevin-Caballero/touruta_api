c84538894184b98e09878050ec9ca834086326cf
Kevin <caballero.kevin94@gmail.com>
2020-10-24 19:31:43 +0200

    re-estructuracion de repositorio

---
+++ src/touruta_infrastructure/Data/Configurations/RoleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Touruta.Core.Data;
+
+namespace Touruta.Infrastructure.Data.Configurations
+{
+    public class RoleConfiguration :IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(e => e.IdRol);
+
+            builder.Property(e => e.IdRol).ValueGeneratedNever();
+
+            builder.Property(e => e.Rol)
+                        .HasMaxLength(50)
+                        .IsUnicode(false);
+        }
+    }
+}
