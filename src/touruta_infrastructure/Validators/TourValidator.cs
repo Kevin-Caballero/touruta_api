397c439c4979017a4742f64c162c1fcfd4d601be
Kevin <caballero.kevin94@gmail.com>
2020-10-25 11:56:51 +0100

    anadida funcionalidad de validacion con fluent

---
+++ src/touruta_infrastructure/Validators/TourValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Touruta.Core.DTOs;
+
+namespace Touruta.Infrastructure.Validators
+{
+    public class TourValidator : AbstractValidator<TourDto> 
+    {
+        public TourValidator()
+        {
+            RuleFor(tour => tour.Description)
+                .NotNull()
+                .Length(10, 1000);
+            RuleFor(tour => tour.Date)
+                .NotNull();
+        }
+    }
+}
