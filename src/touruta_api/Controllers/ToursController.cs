d6e70f7063bd010c04f8e9544341cafcf2ed84a8
Kevin <caballero.kevin94@gmail.com>
2020-10-25 11:55:57 +0100

    añadida funcionalidad de mapeo de entidades

--- src/touruta_api/Controllers/ToursController.cs
+++ src/touruta_api/Controllers/ToursController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Touruta.Core.Data;
+using Touruta.Core.DTOs;
 using Touruta.Core.Interfaces;
 using Touruta.Infrastructure.Repositories;
 
@@ -11,28 +16,34 @@ namespace Touruta.Api.Controllers
     public class ToursController : ControllerBase
     {
         private readonly ITourRepository _tourRepository;
-        public ToursController(ITourRepository tourRepository)
+        private readonly IMapper _mapper;
+
+        public ToursController(ITourRepository tourRepository, IMapper mapper)
         {
             _tourRepository = tourRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetTours()
         {
             var tours = await _tourRepository.GetTours();
-            return Ok(tours);
+            var toursDto = _mapper.Map<IEnumerable<TourDto>>(tours);
+            return Ok(toursDto);
         }
         
         [HttpGet("{id}")]
         public async Task<IActionResult> GetTour(int id)
         {
             var tour = await _tourRepository.GetTour(id);
-            return Ok(tour);
+            var tourDto = _mapper.Map<TourDto>(tour);
+            return Ok(tourDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> PostTour(Tour tour)
+        public async Task<IActionResult> PostTour(TourDto tourDto)
         {
+            var tour = _mapper.Map<Tour>(tourDto);
             await _tourRepository.PostTour(tour);
             return Ok(tour);
         }
---
+++ src/touruta_infrastructure/Mappings/AutomapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Touruta.Core.Data;
+using Touruta.Core.DTOs;
+
+namespace Touruta.Infrastructure.Mappings
+{
+    public class AutomapperProfile: Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Tour, TourDto>();
+            CreateMap<TourDto, Tour>();
+        }
+    }
+}
