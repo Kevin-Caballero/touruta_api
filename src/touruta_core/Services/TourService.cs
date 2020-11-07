2a5c7e798ad96a3b68e53a7afb6fe78e330c89f8
Kevin <caballero.kevin94@gmail.com>
2020-11-08 00:13:06 +0100

    añadidas reglas de negocio

--- src/touruta_core/Services/TourService.cs
+++ src/touruta_core/Services/TourService.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Touruta.Core.Entities;
+using Touruta.Core.Exceptions;
 using Touruta.Core.Interfaces;
 
 namespace Touruta.Core.Services
@@ -15,9 +17,9 @@ namespace Touruta.Core.Services
             _unitOfWork = unitOfWork;
         }
         
-        public async Task<IEnumerable<Tour>> GetTours()
+        public IEnumerable<Tour> GetTours()
         {
-            return await _unitOfWork.TourRepository.GetAll();
+            return _unitOfWork.TourRepository.GetAll();
         }
 
         public async Task<Tour> GetTour(int id)
@@ -27,28 +29,45 @@ namespace Touruta.Core.Services
 
         public async Task PostTour(Tour tour)
         {
+            //Validar que el usuario exista
             var user = await _unitOfWork.UserRepository.GetById(tour.IdUser);
             if (user == null)
             {
-                throw new Exception("User does not exist");
+                throw new BusinessException("User does not exist");
             }
 
+            //Si el usuario tiene menos de 10 tours publicados validamos la fehca para dejarle publicar uno a la semana
+            var userTours = await _unitOfWork.TourRepository.GetToursByUser(tour.IdUser);
+            if (userTours.Count() < 10)
+            {
+                var lastTour = userTours.OrderByDescending(x => x.Date).FirstOrDefault();
+                if ((DateTime.Now - lastTour.Date).TotalDays < 7)
+                {
+                    throw new BusinessException($"You are not able to publish untill {Math.Round(7.0 - (DateTime.Now - lastTour.Date).TotalDays)} days passed");
+                }
+            }
+            
+            //Se lanza una excepcion se en la descripcion se incluye la palabra sexo
+            //TODO: mejorar filtro contenido inadecuado
             if (tour.Description.ToUpper().Contains("SEXO"))
             {
-                throw new Exception("Content not allowed");
+                throw new BusinessException("Content not allowed");
             }
             await _unitOfWork.TourRepository.Add(tour);
+            await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task<bool> PutTour(Tour tour)
         {
-            await _unitOfWork.TourRepository.Update(tour);
+            _unitOfWork.TourRepository.Update(tour);
+            await _unitOfWork.SaveChangesAsync();
             return true;
         }
 
         public async Task<bool> DeleteTour(int id)
         {
             await _unitOfWork.TourRepository.Delete(id);
+            await _unitOfWork.SaveChangesAsync();
             return true;
         }
     }
